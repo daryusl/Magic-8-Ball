11d6aa2db0ecbb572ef0c17da1f90b568fc5774a
daryus <dl_199@hotmail.com>
2019-07-26 15:50:57 -0700

    Add functionality

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var balls: [UIImage] = [UIImage(named: "ball1")!, UIImage(named: "ball2")!, UIImage(named: "ball3")!, UIImage(named: "ball4")!]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var changeBall: UIImageView!
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        
+        let number = Int.random(in: 0 ... 3)
+        
+        changeBall.image = balls[number]
+    }
 
 }
 
